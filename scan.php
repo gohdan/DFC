051eb543745a28dae464cffe2a4c1dbe0fd177b1
Goh'Dan <gohdan@gohdan.ru>
2016-04-21 11:21:21 +0300

    Switched to per-line analysis

--- scan.php
+++ scan.php
@@ -1,14 +1,17 @@
 <?php
 
+//$chars_include = 200;
+$lines_include = 3;
+$max_detection_strlen = 80;
+
 $filename = $argv[1];
-$file_contents = file_get_contents($filename);
+//$file_contents = file_get_contents($filename);
+$file_contents = file($filename, FILE_SKIP_EMPTY_LINES);
 
 //echo ($file_contents."\n");
 
 $patterns_dir = "patterns";
 $detections_dir = "detections";
-
-$chars_include = 200;
 
 if (!file_exists($detections_dir))
 	mkdir($detections_dir);
@@ -20,63 +23,71 @@ foreach ($patterns_files as $pattern_file)
 	if ("." != $pattern_file && ".." != $pattern_file)
 		$patterns[] = parse_ini_file($patterns_dir."/".$pattern_file);
 
-foreach($patterns as $pattern)
-{
-	$pos1 = stripos($file_contents, $pattern['value']);
-	//var_dump ($pos1);
-	$pos2 = mb_stripos($file_contents, $pattern['value']);
-	//var_dump ($pos2);
-
-	if ((false !== $pos1) || (false !== $pos2))
+foreach($file_contents as $line_num => $line)
+	foreach($patterns as $pattern)
 	{
-		//echo ("found!\n");
+		$pos1 = stripos($line, $pattern['value']);
+		//var_dump ($pos1);
+		$pos2 = mb_stripos($line, $pattern['value']);
+		//var_dump ($pos2);
 
-		$det_file_name = $detections_dir."/".$pattern['category']."_".$pattern['name'].".txt";
+		if ((false !== $pos1) || (false !== $pos2))
+		{
+			//echo ("found!\n");
 
-		if (false !== $pos1)
-			$pos = $pos1;
-		else
-			$pos = $pos2;
+			$det_file_name = $detections_dir."/".$pattern['category']."_".$pattern['name'].".php";
 
-		$start = $pos - $chars_include;
-		if ($start < 0)
-			$start = 0;
-		$len = $chars_include * 2;
-		$filelen = strlen($file_contents);
-		if (($start + $len) > $filelen)
-			$len = $filelen - $start;
+			/*
+			if (false !== $pos1)
+				$pos = $pos1;
+			else
+				$pos = $pos2;
 
-		$substring = substr($file_contents, $start, $len);
-		$substring = str_replace($pattern['value'], "!!!!!".$pattern['value']."!!!!!!", $substring);
+			$start = $pos - $chars_include;
+			if ($start < 0)
+				$start = 0;
+			$len = $chars_include * 2;
+			$filelen = strlen($file_contents);
+			if (($start + $len) > $filelen)
+				$len = $filelen - $start;
 
-		$file_part = "============= ".$filename." =============\n\n";
+			$substring = substr($file_contents, $start, $len);
+			$substring = str_replace($pattern['value'], "!!!!!".$pattern['value']."!!!!!!", $substring);
+			*/
 
-		$file_part .= $substring;
-		$file_part .= "\n\n\n\n\n";
+			$line_begin = $line_num - $lines_include;
+			if ($line_begin < 0)
+				$line_begin = 0;
+			$line_end = $line_num + $lines_include;
+			$lines_qty = count($file_contents);
+			if ($line_end >= $lines_qty)
+				$line_end = $lines_qty - 1;
 
-		file_put_contents($det_file_name, $file_part, FILE_APPEND);
+			$lines = "";
+			for ($i = $line_begin; $i <= $line_end; $i++)
+			{
+				$string = $file_contents[$i];
+				if (strlen($string) > $max_detection_strlen)
+				{
+					$line_arr = str_split($string, $max_detection_strlen);
+					$string = implode($line_arr, "\n");
+				}
+				if ($i == $line_num)
+					$lines .= "/* ****** DETECTION ***** */\n";
+				$lines .= $string;
+				if ($i == $line_num)
+					$lines .= "/* ***** end: DETECTION ***** */\n";
+			}
+
+			$file_part = "/* *************** ".$filename." *************** */\n\n";
+
+			//$file_part .= $substring;
+			$file_part .= $lines;
+			$file_part .= "\n\n\n\n\n";
+
+			file_put_contents($det_file_name, $file_part, FILE_APPEND);
+		}
+
 	}
-
-}
-
-
-
-
-
-
-
-	
-
-
-
-
-
-
-
-
-
-
-
-
 
 ?>
