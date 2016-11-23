e863c75f85b1df1168632e98bc1cbda9caee3f99
Goh'Dan <gohdan@gohdan.ru>
2016-11-24 01:12:00 +0300

    Add known files (Wordpress 4.5.4)

--- known_files/hashes/wp-admin/includes/class-wp-upgrader.php
+++ known_files/hashes/wp-admin/includes/class-wp-upgrader.php
@@ -1,3 +1,4 @@
 Wordpress 4.5.3 = f8750b9dd4c272ec3b73c1fef5526aeb
 Wordpress 4.6.1 = a7f16bc0c3baa51f2277b09990cad347
 Wordpress 4.1.13 = ae102033c54341729ccfe40f780a0f6c
+Wordpress 4.5.4 = b8c0d825eddd67a410cb809fa374698d
--- known_files/hashes/wp-includes/version.php
+++ known_files/hashes/wp-includes/version.php
@@ -1,3 +1,4 @@
 Wordpress 4.5.3 = 5880d0c1d88ec5ec0d89f934f5d7b069
 Wordpress 4.6.1 = 230eeba2dceabb8db662a2557e2cceb1
 Wordpress 4.1.13 = f19bbead15fbdcb97ca852b55eb21579
+Wordpress 4.5.4 = 6827a3623cf5ef1c5446320e36601c86
