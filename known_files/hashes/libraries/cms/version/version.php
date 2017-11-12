156897fbe01aecfbe911009ce6db113cce569d0b
Goh'Dan <gohdan@gohdan.ru>
2017-11-12 10:19:28 +0300

    Add known files (Joomla 3.7.5)

--- known_files/hashes/libraries/cms/version/version.php
+++ known_files/hashes/libraries/cms/version/version.php
@@ -26,3 +26,4 @@ Joomla 2.5.19 = 9abfa25bece87b7e326fb100a942eba6
 Joomla 3.0.2 = eb6dd40d791c8cedb3644a000f8ebc98
 Joomla 2.5.14 = b6296a5a5aaba6a54bc66151ecc60c44
 Joomla 3.7.4 = 5d1482866d73bd4b5742785f0e689def
+Joomla 3.7.5 = ed8f7420c4c3bb71846442bdd47ed15f
